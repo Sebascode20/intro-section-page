ddd1c49f7a993eaf587309499ac7295f129266de
Sebascode20 <sebastianemiliotv@gmail.com>
2024-03-22 21:56:55 -0600

    Add dropdown feature items

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const $btnMenuHamburger = document.getElementById("open-menu"),
   $menuExpanded = document.getElementById("menu-expanded"),
   $menuExpanded2 = document.getElementById("menu-expanded-2");
 
+  // Funcion que abre menú hamburguesa
 $btnMenuHamburger.addEventListener("click", () => {
   $navBar.classList.remove("hidden");
   $btnCloseMenuHamburger.classList.add(
@@ -20,13 +21,14 @@ $btnMenuHamburger.addEventListener("click", () => {
   $btnMenuHamburger.classList.add("hidden");
 });
 
+// Funcion que cierra el menú hamburguesa
 $btnCloseMenuHamburger.addEventListener("click", () => {
   $navBar.classList.add("hidden");
   $btnMenuHamburger.classList.remove("hidden");
 });
 
-const opciones = ["Todo List", "Calendar", "Reminders", "Planning"],
- opciones2 = ["History", "Our Team", "Blog"];
+const options = ["Todo List", "Calendar", "Reminders", "Planning"],
+  options2 = ["History", "Our Team", "Blog"];
 
 $menuExpanded.addEventListener("click", () => {
   $menuExpanded.removeChild($menuExpanded.firstElementChild);
@@ -36,17 +38,41 @@ $menuExpanded.addEventListener("click", () => {
   $img.classList.add("inline-block", "px-3");
   $menuExpanded.appendChild($img);
   const $ul = document.createElement("ul"),
-  $fragment = document.createDocumentFragment();
-  opciones.forEach((el) => {
-      const $li = document.createElement("li");
-      const $img2 = document.createElement("img"); 
-      $li.textContent = el;
-      /*$img2.setAttribute("src", "images/icon-todo.svg");
+    $fragment = document.createDocumentFragment();
+
+  //Se añaden los elementos del dropdown Features
+  options.forEach((el) => {
+    const $li = document.createElement("li");
+    const $img2 = document.createElement("img");
+
+    if (el === "Todo List") {
+      $li.textContent = "Todo List";
+      $img2.setAttribute("src", "images/icon-todo.svg");
       $img2.setAttribute("alt", "icon todo list");
-      $li.children[0].appendChild($img2);*/
-      $fragment.appendChild($li);
+      $li.appendChild($img2);
+    } else if (el === "Calendar") {
+      $li.textContent = "Calendar";
+      $img2.setAttribute("src", "images/icon-calendar.svg");
+      $img2.setAttribute("alt", "icon todo list");
+      $li.appendChild($img2);
+    } else if (el === "Reminders") {
+      $li.textContent = "Reminders";
+      $img2.setAttribute("src", "images/icon-reminders.svg");
+      $img2.setAttribute("alt", "icon reminders");
+      $li.appendChild($img2);
+    } else if (el === "Planning") {
+      $li.textContent = "Planning";
+      $img2.setAttribute("src", "images/icon-planning.svg");
+      $img2.setAttribute("alt", "icon planning");
+      $li.appendChild($img2);
+    }
+
+    $li.classList.add("my-4");
+
+    $img2.classList.add("inline", "float-start", "ms-5", "me-5");
+
+    $fragment.appendChild($li);
   });
   $ul.appendChild($fragment);
   $menuExpanded.appendChild($ul);
-})
-
+});
